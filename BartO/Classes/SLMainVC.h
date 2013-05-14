4b35abc09fdcd56aa6727a03f601b39d190dd5dc
Raheel Ahmad <rahmad@fitbit.com>
2013-05-14 15:42:31 -0700

    Initial initial commit

---
+++ BartO/Classes/SLMainVC.h
@@ -0,0 +1,13 @@
+//
+//  SLMainVC.h
+//  BartO
+//
+//  Created by Raheel Ahmad on 5/14/13.
+//  Copyright (c) 2013 Sakun Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLMainVC : UIViewController
+
+@end
---
+++ BartO/Classes/SLMainVC.m
@@ -0,0 +1,38 @@
+//
+//  SLMainVC.m
+//  BartO
+//
+//  Created by Raheel Ahmad on 5/14/13.
+//  Copyright (c) 2013 Sakun Labs. All rights reserved.
+//
+
+#import "SLMainVC.h"
+
+@interface SLMainVC ()
+
+@end
+
+@implementation SLMainVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
