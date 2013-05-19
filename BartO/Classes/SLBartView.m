13caa54b453a8e9004f1b6dd53bdc2aef9debcd5
Raheel Ahmad <rahmad@fitbit.com>
2013-05-18 19:32:18 -0700

    Drawing Bezier paths with touch

--- BartO/Classes/SLBartView.m
+++ BartO/Classes/SLBartView.m
@@ -10,45 +10,102 @@
 
 #define DEGREES_TO_RADIANS(x) ((x * 3.14) / 180)
 
+const NSUInteger MAX_POINTS = 100;
+const NSUInteger ELLIPSE_WIDTH = 4;
+
+@interface SLBartView ()
+@property (nonatomic) BOOL firstTouch;
+@property (nonatomic) BOOL secondTouch;
+@property (nonatomic) BOOL thirdTouch;
+@property (nonatomic) CGPoint *allPoints;
+@property (nonatomic) NSUInteger pointsCount;
+@end
+
 @implementation SLBartView
 
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-	NSLog(@"Touched: %@", touches);
+#pragma mark - Touch handling
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+	CGPoint touchedPoint = [self pointForTouches:touches];
+	
+	if (!self.firstTouch) {
+		CGPoint *pointer = self.allPoints;
+		if (self.pointsCount > 0) {
+			self.pointsCount++;
+		}
+		pointer[self.pointsCount++] = touchedPoint;
+		self.firstTouch = YES;
+		self.secondTouch = NO;
+		self.thirdTouch = NO;
+	} else {
+		self.firstTouch = NO;
+	}
+//	[self setNeedsDisplay]
 }
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
-    return self;
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+	CGPoint touchedPoint = [self pointForTouches:touches];
+	if (self.firstTouch) {
+		self.secondTouch = YES;
+		CGPoint *pointer = self.allPoints;
+		pointer[self.pointsCount++] = touchedPoint;
+	}
 }
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+	CGPoint touchedPoint = [self pointForTouches:touches];
+	if (self.firstTouch && self.secondTouch) {
+		self.thirdTouch = YES;
+		
+		CGPoint *pointer = self.allPoints;
+		pointer[self.pointsCount] = touchedPoint;
+		
+		[self setNeedsDisplay];
+	}
+}
+
+- (CGPoint)pointForTouches:(NSSet *)touches {
+	UITouch *touch = [[touches allObjects] lastObject];
+	return [touch locationInView:self];
+}
+
+#pragma mark - Drawing
 
 - (void)drawRect:(CGRect)rect {
 	CGContextRef myContext = UIGraphicsGetCurrentContext();
 	
-	CGContextSetRGBFillColor(myContext, 0.7f, 0.7f, 0.7f, 1.0f);
+	CGContextSetRGBFillColor(myContext, 0.9f, 0.7f, 0.9f, 1.0f);
 	CGContextFillRect(myContext, CGRectMake(0, 0, CGRectGetWidth(rect), CGRectGetHeight(rect)));
 	
-	UIBezierPath *path = [UIBezierPath bezierPath];
-	[path addArcWithCenter:CGPointMake(200, 200) radius:60 startAngle:0 endAngle:DEGREES_TO_RADIANS(180) clockwise:YES];
-	[path addCurveToPoint:CGPointMake(300, 100) controlPoint1:CGPointMake(250, 60) controlPoint2:CGPointMake(260, 210)];
+	CGContextSetLineWidth(myContext, 2.0f);
+	[[UIColor darkGrayColor] setStroke];
+	[[UIColor blueColor] setFill];
+	CFIndex index = 0;
+	while (index < self.pointsCount) {
+		if (index < self.pointsCount) {
+			CGPoint first = self.allPoints[index];
+			CGContextFillEllipseInRect(myContext, CGRectMake(first.x - ELLIPSE_WIDTH, first.y - ELLIPSE_WIDTH, ELLIPSE_WIDTH * 2, ELLIPSE_WIDTH * 2));
+		}
+		
+		if (index + 2 <= self.pointsCount) {
+			UIBezierPath *path = [UIBezierPath bezierPath];
+			CGPoint first = self.allPoints[index++];
+			CGPoint second = self.allPoints[index++];
+			CGPoint third = self.allPoints[index++];
+			[path moveToPoint:first];
+			[path addQuadCurveToPoint:third controlPoint:second];
+			[path stroke];
+		}
+	}
 	
-	CGContextSaveGState(myContext);
-	
-	CGContextTranslateCTM(myContext, -20, 100);
-	[path stroke];
-	CGContextTranslateCTM(myContext, 0, 40);
-	[[UIColor greenColor] setStroke];
-	[path stroke];
-	
-	CGContextRestoreGState(myContext);
-	
-    CGContextSetRGBFillColor (myContext, 1, 0, 0, 1);// 3
-    CGContextFillRect (myContext, CGRectMake (0, 0, 200, 100 ));// 4
-    CGContextSetRGBFillColor (myContext, 0, 0, 1, .5);// 5
-    CGContextFillRect (myContext, CGRectMake (0, 0, 100, 200));
+}
+
+- (id)initWithFrame:(CGRect)frame {
+	self = [super initWithFrame:frame];
+	if (self) {
+		self.allPoints = calloc(MAX_POINTS, sizeof(CGPoint));
+	}
+	return self;
 }
 
 @end
